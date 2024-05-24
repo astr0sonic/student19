1710bf3f3128eb9f71d762082b4af1df2965c2db
collapsedreams <artsint13@gmail.com>
2024-05-24 13:25:41 +0300

    task7

--- calculator.cpp
+++ calculator.cpp
@@ -1,206 +1,194 @@
 #include "calculator.h"
 #include <cmath>
 #include <stack>
+#include <iostream>
+#include <sstream>
+
+
 
 using namespace std;
 
-int OPEN_BRACKET=0;
-int CLOSE_BRACKET=1;
-int PLUS_OR_MINUS=2;
-int MULT_OR_DIV=3;
-int DEGREE=4;
-int UNARY_MINUS=5;
-
-
-bool isOperator(char symbol)
+int prior(char c)
 {
-    return (symbol == '+' || symbol == '-' || symbol == '*' || symbol == '/' || symbol == '^' || symbol == '~');
-}
-
-int getPriorityOperators(char symbol)
-{
-    int priorityOperator = -1;
-    switch (symbol)
+    switch (c)
     {
-        case '(':
-            priorityOperator = OPEN_BRACKET; 
-            break;
-        case ')':
-            priorityOperator = CLOSE_BRACKET;
-            break;
-        case '+':
-        case '-':
-            priorityOperator = PLUS_OR_MINUS;
-            break;
+        case '^':
+            return 0;
+        case '~':
+            return 1;
         case '*':
         case '/':
-            priorityOperator = MULT_OR_DIV;
-            break;
-        case '^':
-            priorityOperator = DEGREE;
-            break;
+            return 2;
+        case '+':
+        case '-':
+            return 3;
+        case '(':
+            return 4;
         default:
-            priorityOperator = UNARY_MINUS;
-            break;
+            throw exception();
     }
-
-    return priorityOperator;
 }
-
-void getPN(const string& expression, stack<string>* postfixNotation)
+double calculateRPN(istream& in)
 {
-    stack<char> operators;
-    int index = 0, openBracketsCounter = 0;
-    
 
-    for (char symbol : expression) {
-        if (isspace(symbol)) {
-            index++;
-            continue;
+   stack<double> s;
+    while (in.peek() != EOF && in.peek() != '\n')
+    {
+        char c = in.peek();
+        if (isdigit(c))
+        {
+            double d;
+            in >> d;
+            s.push(d);
         }
-        
-        if (isdigit(symbol) || symbol == '.') {
-            string digit = "";
-            while (index < expression.size() && (isdigit(expression[index]) || expression[index] == '.')) {
-                digit += expression[index++];
+        else
+        {
+            double a, b;
+            in.get();
+            switch (c)
+            {
+                case '+':
+                    if (s.size() < 2)
+                        throw exception();
+                    b = s.top(); s.pop();
+                    a = s.top(); s.pop();
+                    s.push(a + b);
+                    break;
+                case '-':
+                    if (s.size() < 2)
+                        throw exception();
+                    b = s.top(); s.pop();
+                    a = s.top(); s.pop();
+                    s.push(a - b);
+                    break;
+                case '*':
+                    if (s.size() < 2)
+                        throw exception();
+                    b = s.top(); s.pop();
+                    a = s.top(); s.pop();
+                    s.push(a * b);
+                    break;
+                case '/':
+                    if (s.size() < 2)
+                        throw exception();
+                    b = s.top(); s.pop();
+                    a = s.top(); s.pop();
+                    s.push(a / b);
+                    break;
+                case '^':
+                    if (s.size() < 2)
+                        throw exception();
+                    b = s.top(); s.pop();
+                    a = s.top(); s.pop();
+                    s.push(pow(a, b));
+                    break;
+                case '~':
+                    if (s.size() < 1)
+                        throw exception();
+                    a = s.top(); s.pop();
+                    s.push(-a);
+                    break;
+                case ' ':
+                    break;
+                default:
+                    throw exception();
             }
-            index--;
-            postfixNotation->push(digit);
         }
-        else if (symbol == '(') {
-            operators.push(symbol);
-            openBracketsCounter++;
-        }
-        else if (symbol == ')') {
-            
-            while (operators.top() != '(') {
-                postfixNotation->push(string(1, operators.top()));
-                operators.pop();
-            }
-            openBracketsCounter--;
-            operators.pop();
-        }
-        else if (isOperator(symbol)) {
-            if (symbol == '-' && (index == 0 || expression[index - 1] == '(')) {
-                operators.push('~');
-            }
-            else {
-                while (!operators.empty() && getPriorityOperators(operators.top()) >= getPriorityOperators(symbol)) {
-                    postfixNotation->push(string(1, operators.top()));
-                    operators.pop();
-                }
-                operators.push(symbol);
-            }
-        }
-        else {
-            return;
-        }
-        index++;
     }
-
-    if (openBracketsCounter > 0) {
-        return;
-    }
-
-    while (!operators.empty()) {
-        postfixNotation->push(string(1, operators.top()));
-        operators.pop();
-    }
+    if (s.size() > 1)
+        throw exception();
+    return s.top();
 }
 
-void reverseStack(stack<string> postfixNotation, stack<string>* reversePostfixNotation)
+double ConvertionToRPN(istream& in)
 {
-    while (!postfixNotation.empty()) {
-        reversePostfixNotation->push(postfixNotation.top());
-        postfixNotation.pop();
-    }
-}
-
-double getExpressionValue(double firstNumber, double secondNumber, string operators)
-{
-    double result = 0.0;
-    if (operators == "+") {
-        result = firstNumber + secondNumber;
-    }
-    else if (operators == "-"){
-        result = firstNumber - secondNumber;
-    }
-    else if (operators == "*") {
-        result = firstNumber * secondNumber;
-    }
-    else if (operators == "/") {
-        if (secondNumber == 0.0) {
-            result=INFINITY;
+    int lastBracket = 0;
+    bool state = false;
+    stringstream out;
+    stack<char> s;
+    while (in.peek() != EOF && in.peek() != '\n')
+    {
+        char c = in.peek();
+        if (isdigit(c))
+        {
+            if (state)
+                throw exception();
+            double d;
+            in >> d;
+            out << d << ' ';
+            state = true;
         }
-        else {
-            result = firstNumber / secondNumber; 
-        }
-    }
-    else if (operators == "^") {
-        result = pow(firstNumber, secondNumber);
-    } 
-    return result;
-}
-
-double calculatePN(stack<string> postfixNotation)
-{
-    stack<double> stackRPN;
-
-    while (!postfixNotation.empty()) {
-
-        string symbol = postfixNotation.top();
-        postfixNotation.pop();
-
-        while(!isalnum(symbol[0]) && !isOperator(symbol[0])) {
-            symbol = postfixNotation.top();
-            postfixNotation.pop();
-        }
-
-        double operand = 0.0;
-        if (isdigit(symbol[0])) {
-            operand = stod(symbol);
-            stackRPN.push(operand);
-        }
-        else if (isOperator(symbol[0])) {
-            if (symbol[0] == '~') {
-                double tempValue = stackRPN.top();
-                stackRPN.pop();
-                tempValue = -tempValue;
-                stackRPN.push(tempValue);
-            }
-            else {
-                if (stackRPN.size() < 2) {
-                    return INFINITY;
-                }
-
-                double firstNumber = stackRPN.top();
-                stackRPN.pop();
-                double secondNumber = stackRPN.top();
-                stackRPN.pop();
-
-                double resultOperations = getExpressionValue(secondNumber, firstNumber, symbol);
-
-                stackRPN.push(resultOperations);
+        else
+        {
+            in.get();
+            switch (c)
+            {
+                case ' ':
+                    break;
+                case '^':
+                case '*':
+                case '/':
+                case '+':
+                    if (!state)
+                        throw exception();
+                    while (!s.empty() && prior(s.top()) <= prior(c))
+                    {
+                        out << s.top() << ' ';
+                        s.pop();
+                    }
+                    s.push(c);
+                    state = false;
+                    break;
+                case '(':
+                    if (state)
+                        throw exception();
+                    lastBracket = in.tellg();
+                    s.push(c);
+                    break;
+                case ')':
+                    if (!state)
+                        throw exception();
+                    while (!s.empty() && s.top() != '(')
+                    {
+                        out << s.top() << ' ';
+                        s.pop();
+                    }
+                    if (s.empty())
+                        throw exception();
+                    s.pop();
+                    break;
+                case '-':
+                    if (!state)
+                        c = '~';
+                    while (!s.empty() && prior(s.top()) <= prior(c))
+                    {
+                        out << s.top() << ' ';
+                        s.pop();
+                    }
+                    s.push(c);
+                    state = false;
+                    break;
+                default:
+                    throw exception();
             }
         }
     }
-
-    if (stackRPN.size() != 1) {
-        return INFINITY;
+    if (!state)
+        throw exception();
+    while (!s.empty())
+    {
+        if (s.top() == '(')
+            throw exception();
+        out << s.top() << ' ';
+        s.pop();
     }
-
-    return stackRPN.top();
+    return calculateRPN(out);
 }
+
+
 
 double calculate(const std::string& expr) {
-    stack<string> stackPostfixNotation;
-    stack <string> reverseStackPostfixNotation;
+    stringstream str(expr);
+    return ConvertionToRPN(str);
 
-    getPN(expr, &stackPostfixNotation);
-
-    reverseStack(stackPostfixNotation, &reverseStackPostfixNotation);
-
-    double result = calculatePN(reverseStackPostfixNotation);
-
-    return result;
 }
